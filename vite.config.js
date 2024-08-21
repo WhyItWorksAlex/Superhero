ccb37b5ab34f67b7f34f72a357b3e47795486dac
WhyItWorksAlex <fishman1q2w3e@gmail.com>
2024-08-21 16:44:41 +0300

    test for netlify

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['./src/setupTests.js'],
-  },
-  // build: {
-  //   rollupOptions: {
-  //     external: ["/src/components/app/app"]
-  //   }
+  // test: {
+  //   globals: true,
+  //   environment: 'jsdom',
+  //   setupFiles: ['./src/setupTests.js'],
   // },
 });
