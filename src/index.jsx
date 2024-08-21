8fb6f22be4152143f56cff42cdf4b0a8f583245b
WhyItWorksAlex <fishman1q2w3e@gmail.com>
2024-08-21 17:14:26 +0300

    test for netlify

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "/src/thems/default";
 import { createRoot } from "react-dom/client";
-import App from "./components/app/app";
+import App from "./components/app/App";
 
 const rootElement = createRoot(document.getElementById("root"));
 rootElement.render(
